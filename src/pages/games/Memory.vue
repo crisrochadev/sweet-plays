95cdd3df73bdd62f7c138e99d7fa7536a58cb912
Cris Rocha <70113780+crisrochadev@users.noreply.github.com>
2023-10-29 17:04:07 -0300

    edit memory

--- src/pages/games/Memory.vue
+++ src/pages/games/Memory.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="w-full h-[calc(100vh_-_150px)]">
     <div class="flex w-full">
-      <div class="flex w-full uppercase font-bold gap-2 justify-between items-center p-2">
-      <p>Pontos: <span class="text-cyan-600 font-extrabold">{{points}}</span></p>
-        <p>Chances: <span class="text-cyan-600 font-extrabold">{{choices}}</span></p>
-    </div>
+      <div
+        class="flex w-full uppercase font-bold gap-2 justify-between items-center p-2"
+      >
+        <p>
+          Pontos: <span class="text-cyan-600 font-extrabold">{{ points }}</span>
+        </p>
+        <p>
+          Chances:
+          <span class="text-cyan-600 font-extrabold">{{ choices }}</span>
+        </p>
+      </div>
     </div>
     <div class="w-full grid gap-1 justify-center items-start p-2 box h-full">
       <button
@@ -35,6 +42,44 @@
         </div>
       </button>
     </div>
+    <div
+      v-if="win"
+      class="w-full h-full fixed bg-[rgba(255,255,255,.25)] top-0 left-0 z-50 flex justify-center items-center"
+    >
+      <div class="p-8 bg-white shadow-md rounded">
+        <h2 class="uppercase font-bold text-cyan-600 mb-8 text-3xl">
+          Parabens!
+        </h2>
+        <p class="">Você encontrou todos</p>
+        <div class="w-full flex justify-center items-center mt-8">
+          <button
+            @click="win = false"
+            class="bg-cyan-600 text-white uppercase p-2 rounded shadow-md hover:bg-cyan-800 cursor-pointer"
+          >
+            Continuar
+          </button>
+        </div>
+      </div>
+    </div>
+    <div
+      v-if="los"
+      class="w-full h-full fixed bg-[rgba(255,255,255,.25)] top-0 left-0 z-50 flex justify-center items-center"
+    >
+      <div class="p-8 bg-white shadow-md rounded">
+        <h2 class="uppercase font-bold text-red-600 mb-8 text-3xl">
+          Que pena!
+        </h2>
+        <p class="">Você perdeu</p>
+        <div class="w-full flex justify-center items-center mt-8">
+          <button
+            @click="los = false"
+            class="bg-red-600 text-white uppercase p-2 rounded shadow-md hover:bg-red-800 cursor-pointer"
+          >
+            Recomeçar
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -55,60 +100,60 @@ export default {
       play,
       erro,
       sucesso,
+      win: false,
+      los: true,
       previous: 0,
       currIcons: icons,
       showing: [],
       completeIcons: [],
       cols: 4,
       par: [],
-      choices:24,
-      points:0,
-      items:[]
+      choices: 48,
+      points: 0,
+      items: [],
     };
   },
   computed: {
     tam() {
       return (window.screen.height - 150) / this.cols;
     },
-    
   },
-  mounted(){
-    this.mountItems()
+  mounted() {
+    this.mountItems();
   },
   methods: {
-    mountItems(){
+    mountItems() {
       let rows = Math.floor((window.screen.height - 150) / this.tam);
 
-        let icons = this.shuffleArray(this.currIcons).filter(
-          (icon, index) =>
-            index > this.previous && index <= (this.next * rows) / 2
-        );
-        let items = [];
-        icons.forEach((item, index) => {
-          items.push({
-            ...item,
-            id: index + 1,
-          });
-          items.push({
-            ...item,
-            id: icons.length + index + 1,
-          });
+      let icons = this.shuffleArray(this.currIcons).filter(
+        (icon, index) =>
+          index > this.previous && index <= (this.next * rows) / 2
+      );
+      let items = [];
+      icons.forEach((item, index) => {
+        items.push({
+          ...item,
+          id: index + 1,
         });
-        this.items = this.shuffleArray(items)
+        items.push({
+          ...item,
+          id: icons.length + index + 1,
+        });
+      });
+      this.items = this.shuffleArray(items);
     },
     showImage(char) {
       this.play();
-      
-      if(this.choices !== 0){
-      if (!this.showing.includes(char)) {
-        this.showing.push(char);
-        this.par.push(char);
-      }
 
-      this.checkWinner1(char);
-      }
-      else{
-        this.loose()
+      if (this.choices !== 0) {
+        if (!this.showing.includes(char)) {
+          this.showing.push(char);
+          this.par.push(char);
+        }
+
+        this.checkWinner1(char);
+      } else {
+        this.loose();
       }
     },
     shuffleArray(array) {
@@ -124,11 +169,11 @@ export default {
           this.completeIcons.push(this.showing[0].char);
           this.completeIcons.push(this.showing[1].char);
           this.checkComplete();
-          this.par = []
-          this.points++
+          this.par = [];
+          this.points++;
         } else {
           this.erro();
-          this.choices--
+          this.choices--;
           setTimeout(() => {
             this.showing = this.showing.filter(
               (s) => s.char !== this.par[1].char && s.char !== this.par[0].char
@@ -138,14 +183,14 @@ export default {
         }
       }
     },
-    loose(){
-      this.points=0
+    loose() {
+      this.points = 0;
       this.par = [];
-          this.showing = [];
-          this.completeIcons = [];
-          this.mountItems()
-          this.erro()
-          alert("Voce nao tem mais chances")
+      this.showing = [];
+      this.completeIcons = [];
+      this.mountItems();
+      this.erro();
+      this.los = true;
     },
     checkComplete() {
       if (this.completeIcons.length === this.items.length) {
@@ -154,9 +199,9 @@ export default {
           this.par = [];
           this.showing = [];
           this.completeIcons = [];
-          this.mountItems()
-          
-          alert("Voce venceu!");
+          this.mountItems();
+
+          this.win = true;
         }, 600);
       }
     },
