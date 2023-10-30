4398d723cd3635411e5682b76aa673429b00fe9f
Cris Rocha <70113780+crisrochadev@users.noreply.github.com>
2023-10-30 18:05:55 -0300

    Update UserSetting.vue

--- src/pages/games/UserSetting.vue
+++ src/pages/games/UserSetting.vue
@@ -1,12 +1,20 @@
 <template>
     <div>
-Configurações
+<div class="w-full h-10 px-4 text-cyan-600 hover:text-cyan-900" @click="store.logout">
+    sair
+</div>
     </div>
 </template>
 
 <script>
+    import {useApi} from "@/stores/index.js"
     export default {
-        
+      data(){
+          const store = useApi()
+          return{
+              store
+          }
+      }  
     }
 </script>
 
