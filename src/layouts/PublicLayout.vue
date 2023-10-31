86c9949acc0c131e25eb01bcb46ff2ed533d5ed9
Cris Rocha <70113780+crisrochadev@users.noreply.github.com>
2023-10-30 23:37:46 -0300

    Update PublicLayout.vue

--- src/layouts/PublicLayout.vue
+++ src/layouts/PublicLayout.vue
@@ -1,8 +1,6 @@
 <template>
     <div class="w-screen h-screen overflow-hidden">
-        <div>
-            <router-link :to="{name:'login'}">Entrar</router-link>
-        </div>
+    
         <router-view :key="$route.path" />
     </div>
 </template>
