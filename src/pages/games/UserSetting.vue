a2e916315bd9470f8528606367466ebb486f4f35
Cris Rocha <70113780+crisrochadev@users.noreply.github.com>
2023-10-30 22:57:04 -0300

    Update UserSetting.vue

--- src/pages/games/UserSetting.vue
+++ src/pages/games/UserSetting.vue
@@ -1,7 +1,7 @@
 <template>
     <div claas="flex flex-col justify-start items-start w-full p-2 gap-2">
-<div class="w-full h-10 px-4 text-white bg-cyan-600 hover:bg-cyan-900 rounded shadow-md" @click="store.logout">
-    sair
+<div class="w-full h-10 px-4 text-white bg-cyan-600 hover:bg-cyan-900 rounded shadow-md flex justify-start items-center gap-2" @click="store.logout">
+  <span class="material-icons-sharp">logout</span>  sair
 </div>
     </div>
 </template>
