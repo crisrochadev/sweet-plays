e0496243decfc304cecc609813fd7aef2ad32b06
Cris Rocha <70113780+crisrochadev@users.noreply.github.com>
2023-10-31 15:29:21 -0300

    add notification push 12

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
-import { createApp, provide } from "vue";
-import "./assets/tailwind.css";
-import "./assets/style.css";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import { createPinia } from "pinia";
-import "material-icons/iconfont/material-icons.css";
+import { createApp , provide} from 'vue'
+import './assets/tailwind.css';
+import './assets/style.css';
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import { createPinia } from 'pinia'
+import 'material-icons/iconfont/material-icons.css';
 
-
-const app = createApp(App).use(createPinia()).use(router);
-app.mount("#app");
+const app = createApp(App)
+.use(createPinia())
+.use(router)
+app.mount('#app')
