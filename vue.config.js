7f0e3f511de48ad540a0e1fb6694781610c906a5
Cris Rocha <70113780+crisrochadev@users.noreply.github.com>
2023-10-31 15:23:38 -0300

    add notification push 8

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,19 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  pwa: {
-    name: "Sweet Plays",
-    themeColor: "#0891b2",
-    msTileColor: "#000000",
-    appleMobileWebAppCapable: "yes",
-    appleMobileWebAppStatusBarStyle: "black",
+  // pwa: {
+  //   name: "Sweet Plays",
+  //   themeColor: "#0891b2",
+  //   msTileColor: "#000000",
+  //   appleMobileWebAppCapable: "yes",
+  //   appleMobileWebAppStatusBarStyle: "black",
 
-    // configure the workbox plugin
-    // workboxPluginMode: 'InjectManifest',
-    // workboxOptions: {
-    //   // swSrc is required in InjectManifest mode.
-    //   swSrc: 'dev/sw.js',
-    //   // ...other Workbox options...
-    // }
-  },
+  //   // configure the workbox plugin
+  //   // workboxPluginMode: 'InjectManifest',
+  //   // workboxOptions: {
+  //   //   // swSrc is required in InjectManifest mode.
+  //   //   swSrc: 'dev/sw.js',
+  //   //   // ...other Workbox options...
+  //   // }
+  // },
 });
