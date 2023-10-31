72bdeac38b25cd0f94588b16f88ce608e7060037
Cris Rocha <70113780+crisrochadev@users.noreply.github.com>
2023-10-31 15:22:08 -0300

    add notification push 7

--- src/main.js
+++ src/main.js
@@ -1,13 +1,23 @@
-import { createApp , provide} from 'vue'
-import './assets/tailwind.css';
-import './assets/style.css';
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import { createPinia } from 'pinia'
-import 'material-icons/iconfont/material-icons.css';
+import { createApp, provide } from "vue";
+import "./assets/tailwind.css";
+import "./assets/style.css";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import { createPinia } from "pinia";
+import "material-icons/iconfont/material-icons.css";
+import "./services/firebase"
 
-const app = createApp(App)
-.use(createPinia())
-.use(router)
-app.mount('#app')
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/firebase-messaging-sw.js")
+    .then((registration) => {
+      console.log("Service Worker registrado com sucesso:", registration);
+    })
+    .catch((error) => {
+      console.error("Erro ao registrar o Service Worker:", error);
+    });
+}
+
+const app = createApp(App).use(createPinia()).use(router);
+app.mount("#app");
