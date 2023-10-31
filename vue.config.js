866b272ee6131704d13ae64a3e2b5f8b84c429ae
Cris Rocha <70113780+crisrochadev@users.noreply.github.com>
2023-10-31 14:54:42 -0300

    add notification push 3

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: {
-    allowedHosts: "all",
-    client: {
-      webSocketURL: "auto://0.0.0.0:0/ws",
-    },
-  },
+  // devServer: {
+  //   allowedHosts: "all",
+  //   client: {
+  //     webSocketURL: "auto://0.0.0.0:0/ws",
+  //   },
+  // },
 });
