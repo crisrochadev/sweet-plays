913bcf7746f725cfb173aa7795f02349a5fa575e
Cris Rocha <70113780+crisrochadev@users.noreply.github.com>
2023-10-31 15:27:05 -0300

    add notification push 10

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -45,12 +45,12 @@
 
 //   self.registration.showNotification(notificationTitle, notificationOptions);
 // });
-self.addEventListener("push", (event) => {
-  const payload = event.data.json();
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: "/img/logo.svg",
-  };
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
+// self.addEventListener("push", (event) => {
+//   const payload = event.data.json();
+//   const notificationTitle = payload.notification.title;
+//   const notificationOptions = {
+//     body: payload.notification.body,
+//     icon: "/img/logo.svg",
+//   };
+//   self.registration.showNotification(notificationTitle, notificationOptions);
+// });
