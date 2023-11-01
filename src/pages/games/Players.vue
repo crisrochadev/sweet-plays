c97a4a2b95734fe069b94ef84437e02f5b7841d7
Cris Rocha <70113780+crisrochadev@users.noreply.github.com>
2023-10-31 23:55:54 -0300

    Update Players.vue20

--- src/pages/games/Players.vue
+++ src/pages/games/Players.vue
@@ -137,7 +137,7 @@ export default {
       return letters;
     },
     addPlayer(player){
-      if(this.store.user){
+    if(this.store.user){
       const notification={ 
         "to": player.messagingToken, 
         "data": { 
@@ -149,22 +149,24 @@ export default {
            } 
         } 
       }
-              
-  const response = await fetch(url, {
-    method: "POST", // *GET, POST, PUT, DELETE, etc.
-    mode: "cors", // no-cors, *cors, same-origin
-    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "same-origin", // include, *same-origin, omit
-    headers: {
-      "Content-Type": "application/json",
-      Authorization:{
+     const url = "https://fcm.googleapis.com/fcm/send"
+     const response = await fetch(url, {
+       method: "POST", // *GET, POST, PUT, DELETE, etc.
+       mode: "cors", // no-cors, *cors, same-origin
+       cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+       credentials: "same-origin", // include, *same-origin, omit
+       headers: {
+          "Content-Type": "application/json",
+           Authorization:{
               key:"AIzaSyBsBNX84OqY8p7o8PDc1_noqercPiGRtdU"
-      }
+        }
       // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: JSON.stringify(notification), // body data type must match "Content-Type" header
-  });
+      },
+      body: JSON.stringify(notification), // body data type must match "Content-Type" header
+     });
+        
       }
+      
     }
   },
 };
